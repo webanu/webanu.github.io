ae843c97f5dbf6118d24aef7c4796cec6b39afed
Your Name <you@example.com>
2014-11-27 12:31:47 +0200

    ok

--- assets/js/contact_me_fi.js
+++ assets/js/contact_me_fi.js
@@ -45,7 +45,7 @@ $(function() {
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Pahoittelumme " + firstName + ", sähköpostipalvelin ei vastaa juuri nyt. Yritäthän myöhemmin uudelleen!");
+                    $('#success > .alert-danger').append("<strong>Pahoittelumme " + firstName + ", sähköpostipalvelin ei vastaa. Yritä myöhemmin uudelleen!");
                     $('#success > .alert-danger').append('</div>');
                     //clear all fields
                     $('#contactForm').trigger("reset");
